6a2c41300bc2a5666eb660d5cc7cbb838ce53975
José Luiz Souza de Abreu <abreu.jls@icloud.com>
2020-07-28 13:38:09 -0300

    Tudo errado!

--- ex078_MaiorMenorValores_Lista.py
+++ ex078_MaiorMenorValores_Lista.py
@@ -1,23 +1,5 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 #  No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
-valores = []
-maior = menor = 0
-
-for p in range(0, 5):
-    valores.append(int(input(f'Digite um valor na posição {p + 1}: ')))
-print(valores)
-
-if p == 0:
-    menor = maior = valores[0]
-else:
-    if valores[p] < menor:
-        menor = valores[p]
-    if valores[p] > maior:
-        maior = valores[p]
-
-print(f'O menor valor digitado foi {menor}')
-print(f'O maior valor digitado foi {maior}')
-
 
 
 
